254afadb4669dbddfeabb760b16d924fb79fdd2e
ankurparanjpe <53052533+ankurparanjpe@users.noreply.github.com>
2020-06-27 12:35:29 +0530

    Update models.py

--- khajana/blog/models.py
+++ khajana/blog/models.py
@@ -2,14 +2,17 @@ from django.db import models
 from django.utils import timezone
 from django.urls import reverse
 from django.contrib.auth.models import User
+from embed_video.fields import EmbedVideoField
 
 
 class Post(models.Model):
     author = models.ForeignKey(User,on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
+    subtitle = models.ForeignKey('blog.SubTitle',related_name='subtitle',on_delete=models.CASCADE,default=1)
     text = models.TextField()
     create_date = models.DateTimeField(blank=True, default=timezone.now)
     pic = models.ImageField(blank=True, default="default.jpeg")
+    video = EmbedVideoField(blank=True)
 
     def approve_comments(self):
         return self.comments.filter(approved_comment=True)
@@ -37,3 +40,20 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.text
+
+
+class Subtitle(models.Model):
+    subs = models.CharField(max_length=200,blank=True)
+
+    def __str__(self):
+        return self.subs
+
+
+class City(models.Model):
+    city = models.CharField(max_length=100)
+
+    def get_absolute_url(self):
+        return reverse('weather')
+
+    def __str__(self):
+        return self.city
