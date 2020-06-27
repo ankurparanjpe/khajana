69cbf3db79ae048e51bf60d7213a8c23928226db
ankurparanjpe <53052533+ankurparanjpe@users.noreply.github.com>
2020-06-27 12:30:31 +0530

    Add files via upload

--- khajana/blog/admin.py
+++ khajana/blog/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Post,Comment
+from .models import Post,Comment,City,Subtitle
 # Register your models here.
 
 admin.site.register(Post)
 admin.site.register(Comment)
+admin.site.register(City)
+admin.site.register(Subtitle)
